3ae7444abc9d2310587754547df3836d7c999ea1
Jerome Mouneyrac <jerome@moodle.com>
2011-09-09 18:20:03 +0800

    missing require config.php

--- facebook_redirect.php
+++ facebook_redirect.php
@@ -4,6 +4,7 @@
  * Get facebook code and call the normal login page
  * Needed to add the parameter authprovider in order to identify the authentication provider
  */
+require('../../config.php');
 $code = optional_param('code', '', PARAM_TEXT); //Google can return an error
 
 if (empty($code)) {
