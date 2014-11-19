b0b5bfc7e4dd64ae08c28cbbb4e3baa6ab4deb27
Jérôme Mouneyrac <jerome@mouneyrac.com>
2014-11-19 15:53:41 +0800

    Fix release number for Moodle.org

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2014060701; // only increment with +1.
 $plugin->requires = 2011070100;   // Requires Moodle 2.1 or later
-$plugin->release = '1.5 (Build: 2013110100) STABLE_25';
+$plugin->release = '1.5 (Build: 2014060701) STABLE_25';
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level
