757fe0348bdd9e99b168c9e44e91a04236ee18ad
Jerome Mouneyrac <jerome@mouneyrac.com>
2015-05-15 03:45:43 -0400

    Add to save the access token

--- db/upgrade.php
+++ db/upgrade.php
@@ -27,11 +27,39 @@
  * @return bool result
  */
 function xmldb_auth_googleoauth2_upgrade($oldversion) {
+    global $DB;
+
+    $dbman = $DB->get_manager(); // loads ddl manager and xmldb classes
 
     if ($oldversion < 2014060700) {
         set_config('oauth2displaybuttons', 0, 'auth/googleoauth2');
         upgrade_plugin_savepoint(true, 2014060700, 'auth', 'googleoauth2');
     }
+
+    if ($oldversion < 2014120102) {
+        
+        // Define table auth_googleoauth2_user_idps to be created.
+        $table = new xmldb_table('auth_googleoauth2_user_idps');
+
+        // Adding fields to table auth_googleoauth2_user_idps.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('provideruserid', XMLDB_TYPE_CHAR, '255', null, null, null, null);
+        $table->add_field('provider', XMLDB_TYPE_CHAR, '255', null, null, null, null);
+        $table->add_field('accesstoken', XMLDB_TYPE_CHAR, '255', null, null, null, null);
+
+        // Adding keys to table auth_googleoauth2_user_idps.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for auth_googleoauth2_user_idps.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Googleoauth2 savepoint reached.
+        upgrade_plugin_savepoint(true, 2014120102, 'auth', 'googleoauth2');
+    }
+
 
     return true;
 }
