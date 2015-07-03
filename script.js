aebebfe0420be1f1fad5f03032e01baccf2649fa
Jerome Mouneyrac <jerome@mouneyrac.com>
2015-07-03 06:31:05 -0400

    Fix undeclare js variable

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(
 
         // CODE HIGHLY INSPIRED FROM ELCENTRA OAUTH2 PLUGIN: https://moodle.org/plugins/view/auth_elcentra
 
-        linkElement = document.createElement("link");
+        var linkElement = document.createElement("link");
         linkElement.rel = "stylesheet";
         linkElement.href = oauth2cssurl;
         document.head.appendChild(linkElement);
@@ -11,18 +11,16 @@ $(document).ready(
         if ($("#auth_custom_location").length > 0) {
             $("#auth_custom_location").append(buttonsCodeOauth2);
         } else {
-            $formObj = $("input[name='username']").closest("form");
-            if ($formObj.length > 0) {
-                $($formObj).each(function (i, formItem) {
-                    $username = $(formItem).find("input[name='username']").val();
-                    $password = $(formItem).find("input[name='password']").val();
-                    if($username!=="guest" || $password!=="guest") {
+            var formObj = $("input[name='username']").closest("form");
+            if (formObj.length > 0) {
+                $(formObj).each(function (i, formItem) {
+                    var username = $(formItem).find("input[name='username']").val();
+                    var password = $(formItem).find("input[name='password']").val();
+                    if(username!=="guest" || password!=="guest") {
                         $(formItem).append(buttonsCodeOauth2);
                     }
                 });
             }
         }
     }
-
-
 )
