ea9dff64d6e10f00bdf19036c4ca97acc87ba9a5
Jerome Mouneyrac <jerome@mouneyrac.com>
2016-11-03 11:53:25 +0000

    Bump version for Moodle.org

--- version.php
+++ version.php
@@ -25,8 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016061200;
+$plugin->version  = 2016061202;
 $plugin->requires = 2014051200;   // Requires Moodle 2.7 or later.
-$plugin->release = '2.3 (Build: 2016061200)';
+$plugin->release = '2.3 (Build: 2016061202)';
 $plugin->maturity = MATURITY_STABLE;             // This version's maturity level.
 $plugin->component = 'auth_googleoauth2'; // Declare the type and name of this plugin.
+
